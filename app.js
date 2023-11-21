c9e4538b78112f0c534f7ddc42b416e32e1462b8
Vildarodinson <117757981+Vildarodinson@users.noreply.github.com>
2023-11-20 20:08:31 -0800

    style: added skills bg color

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ function applyTheme(theme) {
       root.style.setProperty("--text-color", "#555");
       root.style.setProperty("--work-bg", "#f4f4f4");
       root.style.setProperty("--home-bg", "#333");
+      root.style.setProperty("--skills-bg", "#fff");
       root.style.setProperty("--contact-bg", "#f4f4f4");
       root.style.setProperty("--gradient1", "#451f1c");
       root.style.setProperty("--gradient2", "#000000");
@@ -29,6 +30,7 @@ function applyTheme(theme) {
       root.style.setProperty("--text-color", "#333");
       root.style.setProperty("--work-bg", "#ecf0f1");
       root.style.setProperty("--home-bg", "#456");
+      root.style.setProperty("--skills-bg", "#ecf0f1");
       root.style.setProperty("--contact-bg", "#ecf0f1");
       root.style.setProperty("--gradient1", "#362c52");
       root.style.setProperty("--gradient2", "#000000");
@@ -41,6 +43,7 @@ function applyTheme(theme) {
       root.style.setProperty("--text-color", "#fff");
       root.style.setProperty("--work-bg", "#123a54");
       root.style.setProperty("--home-bg", "#1a374a");
+      root.style.setProperty("--skills-bg", "#39818f");
       root.style.setProperty("--contact-bg", "#53758c");
       root.style.setProperty("--gradient1", "#1d3240");
       root.style.setProperty("--gradient2", "#000000");
